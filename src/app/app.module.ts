48b79380e2f94f732565a9548f5b27c5e8818220
Vladislav Krupovich <v.crupovich@dzensoft.com>
2021-10-18 20:42:46 -0400

    Added advanced theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NavBarModule } from './common/modules/nav-bar/nav-bar.module';
+import { OurTeamPageModule } from './pages/our-team-page/our-team-page.module';
+import { ServicesPageModule } from './pages/services-page/services-page.module';
 import { StartPageModule } from './pages/start-page/start-page.module';
 
 @NgModule({
@@ -14,7 +16,9 @@ import { StartPageModule } from './pages/start-page/start-page.module';
     BrowserModule,
     AppRoutingModule,
     NavBarModule,
-    StartPageModule
+    StartPageModule,
+    ServicesPageModule,
+    OurTeamPageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/services-page/services-page.module.ts
+++ src/app/pages/services-page/services-page.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { ServicesPageComponent } from './services-page.component';
 
 
 
 @NgModule({
   declarations: [
-  
-  
     ServicesPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
   ],
   exports: [ServicesPageComponent]
 })
