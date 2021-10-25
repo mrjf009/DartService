1a97c806e2586427d5a4aacab2d0282bdf5b9240
Valentine Tury <mrjf009@gmail.com>
2021-10-25 15:59:19 +0200

    fixed nav-bar.component

--- src/app/pages/our-team-page/our-team-page.module.ts
+++ src/app/pages/our-team-page/our-team-page.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OurTeamPageComponent } from './our-team-page.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactFormModule } from './contact-form/contact-form.module';
 
 @NgModule({
