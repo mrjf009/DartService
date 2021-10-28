009e848e90966f73e3074b44969afd2b4e4d1c17
Valentine Tury <mrjf009@gmail.com>
2021-10-28 13:21:47 +0200

    Added services-page.component

--- src/app/pages/services-page/services-page.component.ts
+++ src/app/pages/services-page/services-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAccordionDisplayMode } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-services-page',
   templateUrl: './services-page.component.html',
-  styleUrls: ['./services-page.component.scss']
+  styleUrls: ['./services-page.component.scss'],
 })
 export class ServicesPageComponent implements OnInit {
+  mode: MatAccordionDisplayMode = 'default';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/services-page/services-page.module.ts
+++ src/app/pages/services-page/services-page.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesPageComponent } from './services-page.component';
-
-
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 
 @NgModule({
-  declarations: [
-  
-  
-    ServicesPageComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [ServicesPageComponent]
+  declarations: [ServicesPageComponent],
+  imports: [CommonModule, MatExpansionModule, BrowserAnimationsModule, CdkAccordionModule],
+  exports: [ServicesPageComponent],
 })
-export class ServicesPageModule { }
+export class ServicesPageModule {}
