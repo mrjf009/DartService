a5b5961b7502db760567909b60f744ac6ade6a6e
LincDi <88975046+LincDi@users.noreply.github.com>
2021-11-08 18:09:44 +0300

    added logic by clicking on the up arrow

--- src/app/pages/services-page/services-page.component.ts
+++ src/app/pages/services-page/services-page.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-services-page',
   templateUrl: './services-page.component.html',
-  styleUrls: ['./services-page.component.scss']
+  styleUrls: ['./services-page.component.scss'],
 })
 export class ServicesPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  elementList: string[] = [
+    'stepperElement1',
+    'stepperElement2',
+    'stepperElement3',
+  ];
+  countClick = 0;
+  outputElementUp = this.elementList[this.countClick];
+  outputElementDown = this.elementList[this.countClick + 1];
+  constructor() {}
+  pressArrowUp() {
+    if (++this.countClick > this.elementList.length - 1) {
+      this.countClick = 0;
+    }
+    this.outputElementUp = this.elementList[this.countClick];
+    this.outputElementDown =
+      this.elementList[
+        this.countClick > this.elementList.length - 2 ? 0 : this.countClick + 1
+      ];
   }
-
+  pressArrowDown() {}
+  ngOnInit(): void {}
 }
