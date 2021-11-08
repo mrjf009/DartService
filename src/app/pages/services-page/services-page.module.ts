ece75822676ad12787c8e6a4adeb97ec55004559
LincDi <88975046+LincDi@users.noreply.github.com>
2021-11-08 18:13:39 +0300

    formatting services-page.module and deleted trash classes

--- src/app/pages/services-page/services-page.module.ts
+++ src/app/pages/services-page/services-page.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesPageComponent } from './services-page.component';
-
-
-
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-  
-  
-    ServicesPageComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [ServicesPageComponent]
+  declarations: [ServicesPageComponent],
+  imports: [CommonModule, MatIconModule],
+  exports: [ServicesPageComponent],
 })
-export class ServicesPageModule { }
+export class ServicesPageModule {}
